2e67735c6b72a369ddff296954240464bd9acd98
nmearl <contact@nicholasearl.me>
2020-06-08 13:37:59 -0400

    Implement draggable usage in new data panel

--- jdaviz/app.vue
+++ jdaviz/app.vue
@@ -4,40 +4,16 @@
       <jupyter-widget :widget="item.widget" v-for="item in state.tool_items" :key="item.name"></jupyter-widget>
       <v-spacer></v-spacer>
       <v-toolbar-items>
-        <v-btn icon @click="state.drawer = !state.drawer">
-          <v-icon v-if="state.drawer">mdi-toy-brick-remove</v-icon>
+        <v-btn icon @click="state.plugin_drawer = !state.plugin_drawer">
+          <v-icon v-if="state.plugin_drawer">mdi-toy-brick-remove</v-icon>
+          <v-icon v-else>mdi-toy-brick-plus</v-icon>
+        </v-btn>
+        <v-btn icon @click="state.data_drawer = !state.data_drawer">
+          <v-icon v-if="state.data_drawer">mdi-toy-brick-remove</v-icon>
           <v-icon v-else>mdi-toy-brick-plus</v-icon>
         </v-btn>
       </v-toolbar-items>
     </v-app-bar>
-
-    <g-sidebar right :visible="state.drawer">
-      <v-card flat tile class="overflow-y-auto fill-height" color="#f8f8f8">
-        <v-expansion-panels accordion multiple focusable flat tile>
-          <v-expansion-panel v-for="(tray, index) in state.tray_items" :key="index">
-            <v-expansion-panel-header>{{ tray.label }}</v-expansion-panel-header>
-            <v-expansion-panel-content>
-              <jupyter-widget :widget="tray.widget"></jupyter-widget>
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-        </v-expansion-panels>
-        <v-divider></v-divider>
-      </v-card>
-    </g-sidebar>
-
-    <g-sidebar :visible="state.drawer">
-      <v-card flat tile class="overflow-y-auto fill-height" color="#f8f8f8">
-        <v-expansion-panels accordion multiple focusable flat tile>
-          <v-expansion-panel v-for="(tray, index) in state.tray_items" :key="index">
-            <v-expansion-panel-header>{{ tray.label }}</v-expansion-panel-header>
-            <v-expansion-panel-content>
-              <jupyter-widget :widget="tray.widget"></jupyter-widget>
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-        </v-expansion-panels>
-        <v-divider></v-divider>
-      </v-card>
-    </g-sidebar>
 
     <v-content
       :style="checkNotebookContext() ? 'height: ' + state.settings.context.notebook.max_height + '; border: solid 1px #e5e5e5;' : ''"
@@ -45,24 +21,73 @@
       <v-container class="fill-height pa-0" fluid>
         <v-row class="fill-height pa-0 ma-0">
           <v-col class="fill-height pa-0 ma-0">
-                          <!-- :style="checkNotebookContext() ? 'height: 100%;' : 'height: calc(100vh - 48px)'" -->
-
-            <golden-layout
-              :has-headers="state.settings.visible.tab_headers"
-              style="height: 100%"
-            >
-              <gl-row :closable="false">
-                <g-viewer-tab
-                  v-for="(stack, index) in state.stack_items"
-                  :stack="stack"
-                  :key="index"
-                  :data-items="state.data_items"
-                  @resize="relayout"
-                  @destroy="destroy_viewer_item($event)"
-                  @data-item-selected="data_item_selected($event)"
-                ></g-viewer-tab>
-              </gl-row>
-            </golden-layout>
+            <!-- :style="checkNotebookContext() ? 'height: 100%;' : 'height: calc(100vh - 48px)'" -->
+            <splitpanes>
+              <pane v-if="state.data_drawer">
+                <v-card flat tile color="#f8f8f8">
+                  <v-list dense class="overflow-y-auto fill-height">
+                    <draggable
+                      v-model="state.data_items"
+                      :group="{name:'data', pull:'clone'}"
+                      style="min-height: 10px"
+                      @start="onDataDragged('start')"
+                      @end="onDataDragged('end')"
+                    >
+                      <template v-for="(data, i) in state.data_items">
+                        <v-list-group prepend-icon="account_circle" value="true" :key="i">
+                          <template v-slot:activator>
+                            <v-list-item-title>{{ data.name }}</v-list-item-title>
+                          </template>
+                          <v-list-item v-for="(child, i) in data.children" :key="i">
+                            <v-list-item-title v-text="child.name"></v-list-item-title>
+                            <!--                        <v-list-item-icon>-->
+                            <!--                          <v-icon v-text="admin[1]"></v-icon>-->
+                            <!--                        </v-list-item-icon>-->
+                          </v-list-item>
+                        </v-list-group>
+                      </template>
+                    </draggable>
+                  </v-list>
+                  <v-divider></v-divider>
+                </v-card>
+              </pane>
+              <pane>
+                <splitpanes>
+                  <pane>
+                    <golden-layout
+                      :has-headers="state.settings.visible.tab_headers"
+                      style="height: 100%"
+                    >
+                      <gl-row :closable="false">
+                        <g-viewer-tab
+                          v-for="(stack, index) in state.stack_items"
+                          :stack="stack"
+                          :key="index"
+                          :data-items="state.data_items"
+                          :dragging="state.dragging_data"
+                          @resize="relayout"
+                          @destroy="destroy_viewer_item($event)"
+                          @data-item-selected="data_item_selected($event)"
+                        ></g-viewer-tab>
+                      </gl-row>
+                    </golden-layout>
+                  </pane>
+                </splitpanes>
+              </pane>
+              <pane v-if="state.plugin_drawer">
+                <v-card flat tile class="overflow-y-auto fill-height" color="#f8f8f8">
+                  <v-expansion-panels accordion multiple focusable flat tile>
+                    <v-expansion-panel v-for="(tray, index) in state.tray_items" :key="index">
+                      <v-expansion-panel-header>{{ tray.label }}</v-expansion-panel-header>
+                      <v-expansion-panel-content>
+                        <jupyter-widget :widget="tray.widget"></jupyter-widget>
+                      </v-expansion-panel-content>
+                    </v-expansion-panel>
+                  </v-expansion-panels>
+                  <v-divider></v-divider>
+                </v-card>
+              </pane>
+            </splitpanes>
           </v-col>
         </v-row>
       </v-container>
@@ -85,6 +110,9 @@ export default {
     checkNotebookContext() {
       this.notebook_context = document.getElementById("ipython-main-app");
       return this.notebook_context;
+    },
+    onDataDragged(e) {
+      this.on_data_dragged(e)
     }
   }
 };
